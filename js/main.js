eee3fe58a964353a7e81fc8b430f38e7258000fb
Soin Roman <soinroma26@gmail.com>
2021-09-13 20:58:49 +0500

    upload project

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+const flowersSlider = new Swiper('.flowers-slider', {
+  // Параметры слайдера
+  loop: true,
+  slidesPerView: 6,
+
+  // Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Параметры слайдера
+  loop: true,
+  slidesPerView: 1,
+
+  // Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+
